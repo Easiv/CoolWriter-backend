d6edea434adfca650334b191c972db11749be88d
Easiv <easiv89@gmail.com>
2018-09-11 16:38:37 +0200

    u can now update and delete resources as well

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module CoolwriterBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options, :patch, :delete]
       end
     end
     
