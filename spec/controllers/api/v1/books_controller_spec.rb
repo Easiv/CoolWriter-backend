b90973091e7fc8e538a9ef2648627829a93caa23
Easiv <easiv89@gmail.com>
2018-10-16 14:56:01 +0200

    controller specs for #index and #show

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 describe Api::V1::BooksController, type: :controller do
-  it 'passes' do
-    expect(true)
+  let!(:book) { create(:book) }
+
+  describe 'index' do
+    subject { get :index }
+
+    it 'returns json' do
+      body = JSON.parse(subject.body)
+      expect(body.length).to eq(1)
+    end
+  end
+  
+  describe 'show' do
+    subject { get :show, params: { id: book.id } }
+
+    it 'returns book json' do
+      formatted_book = JSON.parse subject.body
+      expect(formatted_book['data']['id']).to eq(book.id.to_s)
+    end
   end
 end
