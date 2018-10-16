f1c541c5ec50210800dc5cbe315029a35a025a13
Easiv <easiv89@gmail.com>
2018-10-16 16:17:39 +0200

    controller post spec update

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -14,29 +14,37 @@ describe Api::V1::BooksController, type: :controller do
   
   describe 'show' do
     subject { get :show, params: { id: book.id } }
-
+    
     it 'returns book json' do
       formatted_book = JSON.parse subject.body
       expect(formatted_book['data']['id']).to eq(book.id.to_s)
     end
   end
-
+  
   describe 'create' do
-    subject { post :create,
-      params: { 
-        data: { type: 'books',
+    let(:book_params) do
+      { 
+        data: {
+          type: 'books',
           attributes: { 
-            author: 'aaa', title: 'bbbbb' 
-          } 
+            author: 'aaa',
+            title: 'bbbbb' 
+          }
         } 
       } 
-    }
-    it 'maybe creates book' do
-      expect(subject.body).to include('bbbbb')
     end
-  end
 
-  before do
-    request.headers['Content-Type'] = 'application/vnd.api+json'
+    before do
+      request.headers['Content-Type'] = 'application/vnd.api+json'
+    end
+
+    subject { post :create, params: book_params }
+
+    it 'maybe creates book' do
+      formatted_book = JSON.parse(subject.body)
+      binding.pry
+      expect(formatted_book['data']['attributes']['author']).to eq('aaa')
+      expect(formatted_book['data']['attributes']['title']).to eq('bbbbb')
+    end
   end
 end
