ff02cf45441511a2b3530bd2e05162b6844039d7
Easiv <easiv89@gmail.com>
2018-10-17 14:43:40 +0200

    policies wip

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,6 +2,10 @@ module Api
   module V1
     class BooksController < JSONAPI::ResourceController
       skip_before_action :verify_authenticity_token
+      def destroy
+        @book = Book.find(params[:id])
+        Pundit.authorize(, @book, :book_policy)
+      end
     end
   end
 end
---
+++ app/policies/book_policy.rb
@@ -0,0 +1,12 @@
+class BookPolicy < ApplicationPolicy
+  attr_reader :user, :book
+
+  def initialize(user, book)
+    @user = user
+    @book = book
+  end
+
+  def destroy?
+    user.isAdmin?
+  end
+end
