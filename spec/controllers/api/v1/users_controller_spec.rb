48f2b1d3084962b54adae66df57338ebcba4d485
Easiv <easiv89@gmail.com>
2018-10-18 14:03:11 +0200

    user specs

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,7 +1,83 @@
 require 'rails_helper'
 
 describe Api::V1::UsersController, type: :controller do
-  it 'passes' do
-    expect(true)
+  let!(:user) { create(:user) }
+  let(:body) { JSON.parse(subject.body) }
+
+
+  describe 'index' do
+    subject { get :index }
+
+    it 'checcs if there is a user' do
+      expect(body.length).to eq(1)
+    end
   end
-end
+
+    describe 'show' do
+      subject { get :show, params: { id: user.id } }
+
+      it 'returns requested user' do
+        expect(body['data']['id']).to eq(user.id.to_s)
+      end
+    end
+
+    describe 'create' do
+      let(:user_params) do
+        { 
+          data: {
+            type: 'users',
+            attributes: {
+              name: 'Woooooot',
+              email: 'nice@angery.sigh'
+            }
+          }
+        }
+      end
+
+      before do
+        request.headers['Content-Type'] = 'application/vnd.api+json'
+      end
+
+      subject { post :create, params: user_params }
+
+      it 'returns 2 users' do
+        subject
+        expect(User.all.length).to eq(2)
+      end
+    end
+
+    describe 'update' do
+      let(:user_params) do
+        {
+          id: user.id,
+          type: 'users',
+          attributes: {
+            name: 'luuuul',
+            email: 'ciamciaramcia@dupa.pl'
+          }
+        }
+      end
+
+      before do
+        request.headers['Content-Type'] = 'application/vnd.api+json'
+      end
+
+      subject { patch :update, params: { id: user.id, data: user_params } }
+      it 'passes' do
+        subject
+        expect(user.reload.name).to eq('luuuul');
+        expect(user.reload.email).to eq('ciamciaramcia@dupa.pl');
+      end
+    end
+
+    describe 'destroy' do
+      let!(:juser) { create(:user) }
+      subject { delete :destroy, params: { id: juser.id } }
+
+      it 'removes the juser' do
+        expect { subject }.to change(User, :count).by(-1)
+      end
+    end
+  end
+
+
